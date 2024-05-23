8742ff36a5da9c1d5d4d0bae2c4a2487089d1494
Satak <100216561+Satak-Studios@users.noreply.github.com>
2024-05-23 22:42:36 +0530

    Testing
    Done in 11min 29sec

---
+++ main.js
@@ -0,0 +1,29 @@
+var x, y, a, b, _dist, _speed = 0;
+
+function getFirstPos(){
+    navigator.geolocation.getCurrentPosition(showPositionOld);
+    setInterval(getSecondPos, 1000);
+}
+
+function getSecondPos(){
+    navigator.geolocation.getCurrentPosition(showPositionNew);
+    setInterval(getFirstPos, 1000);
+}
+
+function showPositionOld(position) {
+  x = position.coords.latitude;
+  y = position.coords.longitude;
+}
+
+function showPositionNew(position) {
+    a = position.coords.latitude;
+    b = position.coords.longitude;
+    CalcSpeed();
+}
+
+function CalcSpeed(){
+    _dist = Math.sqrt((x-a)*(x-a) + (y-b)*(y-b));
+    document.getElementById('dist').innerHTML = "Distance : " + _dist;
+    _speed = _dist/10;
+    document.getElementById('speed').innerHTML = _speed + "m/s";
+}
