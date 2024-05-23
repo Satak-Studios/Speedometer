db82d0ef6fa2af700332116cf2be5b22467d7d1c
Satak <100216561+Satak-Studios@users.noreply.github.com>
2024-05-23 23:05:03 +0530

    Debug

--- main.js
+++ main.js
@@ -27,4 +27,27 @@ function CalcSpeed(){
     document.getElementById('dist').innerHTML = "Distance : " + _dist;
     _speed = _dist/10;
     document.getElementById('speed').innerHTML = _speed + "Units";
+    CalcGeoSpeed(x,y,a,b)
 }
+
+function CalcGeoSpeed(lat1, lon1, lat2, lon2) {
+    function toRadians(degrees) {
+        return degrees * Math.PI / 180;
+    }
+    var R = 6371000;
+    var dLat = toRadians(lat2 - lat1);
+    var dLon = toRadians(lon2 - lon1); 
+    var lat1Rad = toRadians(lat1);
+    var lat2Rad = toRadians(lat2);
+    
+    var a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+            Math.sin(dLon / 2) * Math.sin(dLon / 2) * Math.cos(lat1Rad) * Math.cos(lat2Rad);
+    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+    
+    var _dist = R * c;
+    document.getElementById('disto').innerHTML = "Distance: " + _dist + " meters";
+    
+    var _speed = _dist / 10;
+    document.getElementById('speedo').innerHTML = _speed + " m/s";
+}
+
